dd161946952b67d99ed1e44ecfcd913cafaf947c
lucas <lucas.rosaz1@gmail.com>
2015-03-24 12:32:17 +0100

    le vrai create table

---
+++ create_table.sql
@@ -0,0 +1,33 @@
+create table UTILISATEUR(
+	id int PRIMARY KEY,
+	estAdmin int default 0,
+	nom varchar(30) NOT NULL,
+	prenom varchar(30) NOT NULL,
+	login varchar(30) NOT NULL UNIQUE,
+	mdp varchar(30) NOT NULL,
+	dateInscription date);
+
+create table QUESTION(
+	id int PRIMARY KEY,
+	libelle varchar(30));
+
+create table QUESTIONNAIRE(
+	id int PRIMARY KEY,
+	titre varchar(30),
+	idQ int,
+	CONSTRAINT fk1 FOREIGN KEY (idQ) REFERENCES QUESTION(id));
+
+create table STATSHISTORIQUE(
+	id int PRIMARY KEY,
+	intitule varchar(30),
+	valeur int);
+
+create table PROPOSITION(
+	id int PRIMARY KEY,
+	ordre int,
+	libelle varchar(30));
+
+create table REPONSE(
+	idR int PRIMARY KEY,
+  	constraint fk1 FOREIGN KEY (idR) REFERENCES PROPOSITION(id),
+	dateR date);
